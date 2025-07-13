424bffd57afc3fd304c372700cba80aa5dced9fe
Tina <nguyen.valentina.99@gmail.com>
2025-07-13 14:49:26 +0200

    fixed main usage message

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 #include "../includes/cgi/CgiHandler.hpp"
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) return (std::cout << "Usage: " << argv[0] << " <config_file> \n", 1);
+    if (argc != 2) return (std::cout << "Usage:	 " << argv[0] << " <config_file> \nExample: " << argv[0] << " config/regular.conf\n", 1);
     try {
         ServerConfig serverConfig(argv[1]);
         ServerManager allServers(serverConfig);
